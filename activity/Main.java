364bbe9f50ba029c242e2c47c5911822297ed595
Vinesh Kannan <vingkan@gmail.com>
2017-07-11 21:20:45 +0000

    Simple example

--- activity/Main.java
+++ activity/Main.java
@@ -7,40 +7,22 @@ import tools.Marker;
 
 public class Main {
     
-    public static final Marker IIT = new Marker(
-        41.8350991, -87.6276149,
-        "Illinois Institute of Technology",
-        Color.BLACK, 5);
-        
     public static void main(String[] args) {
         
-        if (args.length != 2) {
-            throw new Error("Improper arguments.");
-        }
-        
-        String filename = args[0];
-        String outfile = args[1];
+        String filename = "files/inspections.txt";
+        String outfile = "files/markers.txt";
 
         List<Inspection> inspections = Helper.readInspectionsFromFile(filename);
         List<Marker> markers = new ArrayList<Marker>();
-        markers.add(IIT);
             
         for (int i = 0; i < inspections.size(); i++) {
             
             Inspection ins = inspections.get(i);
             
             Marker marker = new Marker(ins.getLat(), ins.getLng());
-            String text = ins.getName() + ": " + ins.getNotes();
+            String text = ins.getName();
             marker.setText(text);
-            if (ins.passed()) {
-                marker.setColor(Color.GREEN);
-            }
-            if (ins.isHighRisk()) {
-                marker.setSize(10);
-                marker.setColor(Color.RED);
-            } else {
-                marker.setSize(5);
-            }
+            marker.setSize(5);
             markers.add(marker);
             
         }
@@ -49,6 +31,5 @@ public class Main {
         Helper.closeAllFiles(); // Don't forget this! Otherwise nothing will write.
         
     }
-
 
 }
