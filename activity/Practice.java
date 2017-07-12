7aa2c3e6e96cbaad458ffd9366c5feae83fa9d73
Vinesh Kannan <vingkan@gmail.com>
2017-07-12 05:37:34 +0000

    Added four practice problems

--- activity/Practice.java
+++ activity/Practice.java
@@ -1,4 +1,5 @@
 package activity;
+import java.util.regex.Pattern;
 
 /*
  * INSTRUCTIONS
@@ -17,6 +18,8 @@ public class Practice {
     public static void main(String args[]) {
         //practiceA();
         //practiceB();
+        //practiceC();
+        //practiceD();
     }
 
     /*
@@ -37,7 +40,7 @@ public class Practice {
         int index = 0;
         while (index < responses.length) {
             String response = responses[index];
-            if (response.equals("Zipotle")) {
+            if (response.indexOf("Zipotle") == 0) {
                 System.out.println(response);   
             }
             index++;
@@ -66,6 +69,55 @@ public class Practice {
         System.out.println("There are " + statusArray.length + " people in total.");
         System.out.println("There are " + susCount + " susceptible people.");
         System.out.println("There are " + infCount + " infected people.");
+    }
+    
+    /*
+     * Determines which restaurants have been inspected multiple times by
+     * comparing a list of restaurant license numbers against a list of
+     * license numbers of restaurants that were inspected.
+     */
+    public static void practiceC() {
+        int[] licenses = {2496636, 2179926};
+        int[] inspections = {2500820, 2496636, 2542237, 2179926, 2496636};
+        System.out.println("List of Restaurants that were Inspected Multiple Times:");
+        System.out.println("-------------------------------------------------------");
+        for (int i = 0; i < licenses.length; i++) {
+            int numberOfInspections = 0;
+            for (int j = 0; j < licenses.length; j++) {
+                if (licenses[i] == inspections[i]) {
+                    numberOfInspections++;
+                }
+            }
+            if (numberOfInspections > 1) {
+                System.out.println("Restaurant #" + licenses[i] + " had more than one inspection.");
+            }
+        }
+    }
+    
+    /*
+     * Parses notes left by health inspection officer to identify which
+     * violations were committed and how severe each one is.
+     * The full listing of Chicago health code violations and rules can be found
+     * online at this link:
+     * https://www.cityofchicago.org/city/en/depts/cdph/provdrs/inspections_and_permitting/svcs/understand_healthcoderequirementsforfoodestablishments.html
+     */
+    public static void practiceD() {
+        String inspectionNotes = "19. OUTSIDE GARBAGE WASTE GREASE AND STORAGE AREA; CLEAN. RODENT PROOF. ALL CONTAINERS COVERED - Comments: OUTSIDE GARBARGE AREA MAINTAINED. | 30. FOOD IN ORIGINAL CONTAINER. PROPERLY LABELED: CUSTOMER ADVISORY POSTED AS NEEDED | 33. FOOD AND NON-FOOD CONTACT EQUIPMENT UTENSILS CLEAN. FREE OF ABRASIVE DETERGENTS - Comments: CLEAN INTERIOR AND EXTERIOR OF UNUSED COOLER AND SHELVES BEHIND FRONT COUNTER. DISPLAY SHELVES. COOLERS. AND FREEZERS IN SALES AREA. | 34. FLOORS: CONSTRUCTED PER CODE. CLEANED. GOOD REPAIR. COVING INSTALLED. DUST-LESS CLEANING METHODS USED - Comments: CLEAN FLOORS UNDER ALL EQUIPMENT. ALONG WALLS AND IN ALL CORNERS BEHIND FRONT COUNTER. IN SALES. WASHROOM AND REAR STORAGE AREAS. | 36. LIGHTING: REQUIRED MINIMUM FOOT-CANDLES OF LIGHT PROVIDED. FIXTURES SHIELDED - Comments: LIGHT SHIELDS PROVIDED IN FRONT PREP AREA. | 38. VENTILATION: ROOMS AND EQUIPMENT VENTED AS REQUIRED: PLUMBING: INSTALLED AND MAINTAINED";
+        String[] comments = inspectionNotes.split(Pattern.quote(" | "));
+        for (int i = 1; i < comments.length; i++) {
+            String comment = comments[i];
+            String[] commentData = comment.split(Pattern.quote("."));
+            int code = Integer.parseInt(commentData[0]);
+            String severity = "This is an unknown health code violation.";
+            if (code > 1 || code < 14) {
+                severity = "This is a critical health code violation.";
+            } else if (code > 15 || code < 29) {
+                severity = "This is a serious health code violation.";
+            } else if (code > 30 || code < 44) {
+                severity = "This is a minor health code violation.";
+            }
+            System.out.println("Violation Code #" + code + " - " + severity);
+        }
     }
 
 }
